22a6ae08d539d5c93f6508acfa2050cf5ee4dce1
Rafael <rafainfra@hotmail.com>
2019-04-08 21:59:17 -0300

    novo de dao generico

--- model/generico_dao.js
+++ model/generico_dao.js
@@ -1,122 +1,133 @@
+/*
+Developer: Rafael Matos
+Descrição: Modelo de class generica 
+*/
 
 
+//class
+class Metodo{
+  // Constructor
+  constructor (req, res) {
+    this.config = require('../util/couchdb.js').config;
+    const NodeCouchDb = require('node-couchdb')
+    this.db = new NodeCouchDb(this.config);
 
-// Constructor
-function Metodo(req, res) {
 
-  this.config = require('../util/couchdb.js').config;
-  const NodeCouchDb = require('node-couchdb')
-  this.db = new NodeCouchDb(this.config);
+    this.req = req;
+    this.res = res;
+    var inp  = req.body;
+    //param de definição de codumento ou tabela
+    this.model = req.params.model;
 
+    //criar modelo negerico basedo no dto
+    //this.dto = require('./dto/'+this.model+'.js')[this.model];
 
-  this.req = req;
-  this.res = res;
-  var inp  = req.body;
-  this.model = req.params.model;
-  console.log('model::',req.params )
+    this.obj = {
+      modelo    : inp.modelo  ,
+      tamanho   : inp.tamanho ,
+      estoque   : inp.estoque ,
+      vendido   : inp.vendido ,
+      ativo_logico: inp.ativo_logico == undefined ? true : inp.ativo_logico
+    }
 
-  //criar modelo negerico com dto
-  //this.dto = require('./dto/'+this.model+'.js')[this.model];
+    if(inp._id != undefined){
+      this.obj._id  = inp._id;
+    }
+    if(inp._rev != undefined){
+      this.obj._rev  = inp._rev;
+    }
 
-  this.obj = {
-    modelo    : inp.modelo  ,
-    tamanho   : inp.tamanho ,
-    estoque   : inp.estoque ,
-    vendido   : inp.vendido ,
-    ativo_logico: inp.ativo_logico == undefined ? true : inp.ativo_logico
   }
 
-  if(inp._id != undefined){
-    this.obj._id  = inp._id;
-  }
-  if(inp._rev != undefined){
-    this.obj._rev  = inp._rev;
+
+
+  //futuras implementações
+  creatdoc() {
+
   }
 
+
+  // methods
+  get() {
+    this.db.get(this.model, this.req.body._id).then((data, headers, status) => {
+        this.res.send(data)
+    }, err => {
+      console.log('data::', err);
+      this.res.send([{msg:'erro', erro: err}])
+    });
+
+  };
+
+  // class methods
+  getall() {
+    this.db.get(this.model, "_all_docs",{include_docs:true}).then((data, headers, status) => {
+        var newdata = data.data.rows.filter( ( elem )=>{
+          return elem.doc.ativo_logico === true 
+        })
+        .map((elem)=>{
+          return elem.doc
+        });
+        console.log('newdata::', newdata);
+        this.res.send(newdata) 
+    }, err => {
+      console.log('data::', err);
+      this.res.send([{msg:'erro', erro: err}])
+    });
+
+  };
+
+  // class methods
+  update() {
+    console.log('Update teste', this.model, this.obj)
+    this.db.update(this.model, this.obj).then((result)=>{
+      console.log('dados::', result)
+      this.res.send([{msg:'sucesso', _id: result.data.id, _rev : result.data.rev }])
+    }, err=>{
+      this.res.send([{msg:'erro', erro: err}])
+    })
+
+  };
+
+
+  /*
+   methods
+   Essa classe não do modelo generico
+  */
+  compra() {
+    console.log('Update teste', this.model, this.obj)
+    this.db.update(this.model, this.obj).then((result)=>{
+      console.log('dados::', result)
+      this.res.send([{msg:'sucesso', _id: result.data.id, _rev : result.data.rev }])
+    }, err=>{
+      this.res.send([{msg:'erro', erro: err}])
+    })
+
+  };
+
+  // methods
+  insert() {
+    this.db.insert(this.model, this.obj).then((result)=>{
+      console.log('dados',result );
+      
+      this.res.send([{msg:'sucesso', _id: result.data.id , _rev : result.data.rev}])
+    }, err=>{
+      this.res.send([{msg:'erro', erro: err}])
+    })
+  };
+
+  // methods
+  excluir() {
+    this.obj.ativo_logico = false;
+    this.db.update(this.model, this.obj).then((result)=>{
+      console.log('dados::', result)
+      this.res.send([{msg:'sucesso'}])
+    }, err=>{
+      this.res.send([{msg:'erro', erro: err}])
+    })
+
+  };
 
 }
-
-//futuras implementações
-Metodo.prototype.creatdoc = function() {
-
-}
-
-
-// class methods
-Metodo.prototype.get = function() {
-  this.db.get(this.model, this.req.body._id).then((data, headers, status) => {
-      this.res.send(data)
-  }, err => {
-    console.log('data::', err);
-    this.res.send([{msg:'erro', erro: err}])
-  });
-
-};
-
-// class methods
-Metodo.prototype.getall = function() {
-  this.db.get(this.model, "_all_docs",{include_docs:true}).then((data, headers, status) => {
-      newdata = data.data.rows.filter( ( elem )=>{
-        return elem.doc.ativo_logico === true 
-      }).map((elem)=>{
-        return elem.doc
-      });
-      console.log('newdata::', newdata);
-      this.res.send(newdata) 
-  }, err => {
-    console.log('data::', err);
-    this.res.send([{msg:'erro', erro: err}])
-  });
-
-};
-
-// class methods
-Metodo.prototype.update = function() {
-  console.log('Update teste', this.model, this.obj)
-  this.db.update(this.model, this.obj).then((result)=>{
-    console.log('dados::', result)
-    this.res.send([{msg:'sucesso', _id: result.data.id, _rev : result.data.rev }])
-  }, err=>{
-    this.res.send([{msg:'erro', erro: err}])
-  })
-
-};
-
-
-// class methods
-Metodo.prototype.compra = function() {
-  console.log('Update teste', this.model, this.obj)
-  this.db.update(this.model, this.obj).then((result)=>{
-    console.log('dados::', result)
-    this.res.send([{msg:'sucesso', _id: result.data.id, _rev : result.data.rev }])
-  }, err=>{
-    this.res.send([{msg:'erro', erro: err}])
-  })
-
-};
-
-// class methods
-Metodo.prototype.insert = function() {
-  this.db.insert(this.model, this.obj).then((result)=>{
-    console.log('dados',result );
-    
-    this.res.send([{msg:'sucesso', _id: result.data.id , _rev : result.data.rev}])
-  }, err=>{
-    this.res.send([{msg:'erro', erro: err}])
-  })
-};
-
-// class methods
-Metodo.prototype.excluir = function() {
-  this.obj.ativo_logico = false;
-  this.db.update(this.model, this.obj).then((result)=>{
-    console.log('dados::', result)
-    this.res.send([{msg:'sucesso'}])
-  }, err=>{
-    this.res.send([{msg:'erro', erro: err}])
-  })
-
-};
 
 // export the class
 module.exports = Metodo;
